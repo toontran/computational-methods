07b72ad1094eac2a1ba77d632d23fd75968a2257
Toon Tran <sontungtran99@gmail.com>
2026-03-07 18:40:45 -0500

    quantifying leftout test - check output

--- test.py
+++ test.py
@@ -12,8 +12,9 @@ print(data['current_total'], data['throw'], data['iteration'])
 
 iteration = 49
 dir_path = "output/kernel_stocks_5000_10.0_isvd_random_uniform_size_100_k_10_reservoir_greedy/"
-file_path = os.path.join(dir_path, f'spectrum_data_{iteration}.npz')
+file_path = os.path.join(dir_path, f'leftout_data_{iteration}.npz')
 # try:
 data = np.load(file_path, allow_pickle=True)
+import pdb; pdb.set_trace()
 print(data['current_total'], data['throw'], data['iteration'])
     
