5c3a59c2310da1b8e6bb7e68b8861aa91c757516
Toon Tran <sontungtran99@gmail.com>
2026-03-07 18:27:09 -0500

    quantifying leftout test - check output

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+import numpy as np
+
+# for iteration in range(last_available_file_number+1):
+dir_path = "~/pj/computational-methods/output/kernel_stocks_5000_10.0_isvd_random_uniform_size_100_k_10_reservoir_greedy/"
+file_path = os.path.join(dir_path, f'spectrum_data_{iteration}.npz')
+try:
+    data = np.load(file_path, allow_pickle=True)
+    print(data[''])
+    
