b4b0619d67df1683ade909fdd3db2889fe158483
Toon Tran <sontungtran99@gmail.com>
2026-03-11 17:01:42 -0400

    rerun win v sketch kernel 27 machines

--- main.py
+++ main.py
@@ -982,10 +982,10 @@ if __name__ == "__main__":
         # "decreasing_V2": np.argsort(np.abs(Vt_exact[1,:]))[::-1],
         # "manual_perm": perm,
         # "manual_perm_reverse": perm[::-1],
-        # "random_uniform_2": random_uniform_perms[1],
-        # "random_uniform_3": random_uniform_perms[2],
-        # "random_uniform_4": random_uniform_perms[3],
-        # "random_uniform_5": random_uniform_perms[4],
+        "random_uniform_2": random_uniform_perms[1],
+        "random_uniform_3": random_uniform_perms[2],
+        "random_uniform_4": random_uniform_perms[3],
+        "random_uniform_5": random_uniform_perms[4],
         # "random_wr": random_uniform_streams[0],
     }
 
@@ -998,7 +998,7 @@ if __name__ == "__main__":
     # raise
     save_mat = False
     no_og = False
-    no_adaptive = True 
+    no_adaptive = False 
     if save_mat:
         import scipy
     
