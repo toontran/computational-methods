4a3a87efff4815de50e93f2b4e34c446c36eaa34
Toon Tran <sontungtran99@gmail.com>
2026-03-17 16:18:58 -0400

    first 4, fix sketch

--- run_tests_fix_sketch_vary_win.sh
+++ run_tests_fix_sketch_vary_win.sh
@@ -15,27 +15,27 @@ k_values=(
 #     4
 #     16
 #     32
-#     128
+#     1 28
 # )
 
 
 # List of matrices
 matrices=(
-    # "kernel_stocks_1000_0.7071"
-    # "kernel_stocks_1000_1.0"
-    # "kernel_stocks_1000_2.2361"
-    # "bad_case1_1000"
+    "kernel_stocks_1000_0.7071"
+    "kernel_stocks_1000_1.0"
+    "kernel_stocks_1000_2.2361"
+    "bad_case1_1000"
     # "bad_case2_1000"
     # "bad_case3_1000"
     # "FIDAP/ex3"
     # "HB/plat1919"
-    "HB/1138_bus"
+    # "HB/1138_bus"
 )
 
 # Window sizes to sweep
 win_sizes=(
-    1
-    4
+    # 1
+    # 4
     32
     128
     256
